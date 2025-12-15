c1668300957ebdb6dfb78156e0ec622cba88f789
antonin <anto73grillet@gmail.com>
2025-12-15 19:02:24 +0100

    fix resend 2

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
 
 		// Envoyer l'email avec Resend
 		const { data, error } = await resend.emails.send({
-			from: "Portfolio <onboarding@resend.dev>", // Email vérifié sur Resend
+			from: "Portfolio Antonin <contact@gchb.fr>",
 			to: "anto73grillet@gmail.com",
 			replyTo: email,
 			subject: `Nouveau message de ${name} - Portfolio`,
